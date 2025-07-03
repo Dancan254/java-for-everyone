8c598994e42464c600e5d30efc4ea47a2e26a044
Dancan254 <dancanian25@gmail.com>
2025-07-03 23:19:38 +0300

    add: implement VariableSwap class to demonstrate variable swapping in Java

---
+++ src/introduction/VariableSwap.java
@@ -0,0 +1,18 @@
+package introduction;
+
+public class VariableSwap {
+
+    public static void main(String[] args) {
+
+        int a = 90;
+        int b = 80;
+        System.out.println("a = " + a + " b = " + b);
+
+        int temp = a;
+        a = b;
+        b =temp;
+
+        System.out.println("After Swapping, the !magic happens");
+        System.out.println("a = " + a + " b = " + b);
+    }
+}
