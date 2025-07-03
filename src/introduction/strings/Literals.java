2ee637e45bb7860e32186620e9a9b95351fb2c06
Dancan254 <dancanian25@gmail.com>
2025-07-03 23:19:44 +0300

    add: create Literals class to demonstrate string literals and formatting in Java

---
+++ src/introduction/strings/Literals.java
@@ -0,0 +1,24 @@
+package introduction.strings;
+
+import java.util.jar.JarOutputStream;
+
+public class Literals {
+
+    public static void main(String[] args) {
+
+        String simple = "Hello World";
+        String withEscape = "He said, \"Hello!\"";
+        String multiLine = "Line 1\nLine 2\nLine 3";
+        // Line 1
+        // Line 2
+        //Line 3
+
+        String newLine = "1. Ian\n2. Mongare\n3. Ojo";
+        String space = "Ian\tMongare\tOjo";
+        System.out.println(space);
+        System.out.println(newLine);
+        //System.out.println(withEscape);
+
+        System.out.print("New space, when I print out stuff");
+    }
+}
