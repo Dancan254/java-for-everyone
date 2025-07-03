7852a70bfc8d81960ed1b63e870f2bc80b5ace98
Dancan254 <dancanian25@gmail.com>
2025-07-03 23:19:32 +0300

    add: implement Rectangle class to calculate area from user input

---
+++ src/introduction/Rectangle.java
@@ -0,0 +1,29 @@
+package introduction;
+
+import java.util.Scanner;
+
+public class Rectangle {
+
+    public static void main(String[] args) {
+        //create a program that calculates the area of a rectangle from user input
+        //create an object of scanner class
+        Scanner input = new Scanner(System.in);
+        //prompt user to enter the first value-  length
+        System.out.println("Welcome to our first toy cli project");
+        System.out.println("=".repeat(26));
+        System.out.println("Enter the length: ");
+        //use the nextInt() method to read in
+        int length = input.nextInt();
+        //ask for the  width
+        System.out.println("Enter the width: ");
+        //use the nextInt to read the width
+        int width = input.nextInt();
+        //calc area = l * w
+        int area = length * width;
+        //output
+        System.out.println("You entered " + length + " and " + width);
+        System.out.println("The area of the rectangle is " + area);
+
+        input.close();
+    }
+}
