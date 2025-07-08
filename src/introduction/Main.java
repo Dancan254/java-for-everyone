f346cd6e7c645fc7c9bbb638f18cb318ffa4eec6
Dancan254 <dancanian25@gmail.com>
2025-07-08 08:35:55 +0300

    main fix

--- src/introduction/Main.java
+++ src/introduction/Main.java
@@ -1,13 +1,34 @@
 package introduction;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-
         //obj1
-        Simple obj1 = new Simple("Ian");
-        Simple obj2 = new Simple("Ian");
+//        Simple obj1 = new Simple("Ian");
+//        Simple obj2 = new Simple("Ian");
+//
+//        System.out.println("Obj1 " + System.identityHashCode(obj1));
+//        System.out.println("Obj2 " + System.identityHashCode(obj2));
 
-        System.out.println("Obj1 " + System.identityHashCode(obj1));
-        System.out.println("Obj2 " + System.identityHashCode(obj2));
+        //anagrams
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        List<List<String>> result = groupAnagrams(strs);
+        System.out.println(result);
+
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            //get the characters
+            char[] arr = str.toCharArray();
+            //sort the chars - aet
+            Arrays.sort(arr);
+            String key = new String(arr);
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(str);
+        }
+        return new ArrayList<>(map.values());
     }
 }
